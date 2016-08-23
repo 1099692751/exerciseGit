c2827e9c24e1bc351fc7e051998324c95ea92507
yangMac <1099692751@qq.com>
2016-08-23 14:54:54 +0800

    第一次提交github

---
+++ exerciseGithub/exerciseGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  exerciseGithub
+//
+//  Created by Mac on 16/8/23.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ exerciseGithub/exerciseGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  exerciseGithub
+//
+//  Created by Mac on 16/8/23.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
